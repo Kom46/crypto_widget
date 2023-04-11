76ac8046fbb8210fe99853a41102ee5edb1b915c
kom46 <kommunizm46@gmail.com>
2023-04-11 20:56:42 +0300

    added basic yahoo finance API usage

--- finance.py
+++ finance.py
@@ -1,19 +1,60 @@
+from collections import namedtuple
+from typing import Self
 import yfinance as yf
 import json as js
 import datetime as dt
 
-class yahooClient:
-    def __init__(self) -> None:
-        pass
-    
-    def getTickets(tickets = any, start = dt.date(), end = dt.date()):
+
+class basicClient:
+    def __new__(cls) -> 'basicClient':
         pass
 
+    def __init__(self) -> None:
+        pass
+
+    def getTicket(self, ticket: str = None):
+        data = None
+        if ticket != None:
+            data = ticket
+        return data
+
+    def getTickets(self, tickets=any, start=dt.date(), end=dt.date()):
+        data = []
+        ticketClass = namedtuple('Ticket', ['name', 'ticketData'])
+        for ticket in tickets["tickets"]:
+            data.append(ticketClass(
+                ticket["name"], self.getTicket(ticket["ticket"])))
+        return data
+
+
+class yahooClient(basicClient):
+    def __init__(self) -> None:
+        pass
+
+    def __new__(cls) -> 'yahooClient':
+        return super().__new__()
+    
+    def getTicket(self, ticket: str = None, start = dt.datetime.now(), end = dt.datetime.now()):
+        result = super().getTicket(ticket)
+        if result != None:
+            ticker = yf.Ticker(ticket)
+            data = ticker.history(start=start, end=end)
+            price = data["Close"][0]
+            result = price, data
+        return result
+
+
+stockClients = {"yahoo": yahooClient}
+
+
 class stockExchange:
-    def __init__(self, name = None, tickets = [], api_key = None) -> None:
+    def __init__(self, name=None, tickets=[], api_key=None) -> None:
         self.name = name
         self.api_key = api_key
         self.tickets = tickets
+        if name != None:
+            self.client = stockClients.get(name).__new__()
+
 
 class stockClient:
     def __init__(self) -> None:
@@ -22,6 +63,5 @@ class stockClient:
         with open("parms.json", "r") as fd:
             config = js.load(fp=fd)
             for st in config["stock_exchange"]:
-                self.stock = stockExchange(st["name"], st["tickets"], st["api_key"])
-                
-                
+                self.stock = stockExchange(
+                    st["name"], st["tickets"], st["api_key"])
