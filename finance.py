447ad596d38b46c425fbe4d06c6822d7f1d6fc5f
kom46 <kommunizm46@gmail.com>
2023-05-02 09:59:50 +0300

    some refactoring added; coinmarketcap implementation started;

--- finance.py
+++ finance.py
@@ -10,11 +10,25 @@ from kucoin.exceptions import *
 class basicClient:
     tickers = []
     
+    def __init__(self, config = None, api_key = "") -> None:
+        self.__api_key  = api_key
+    
+    @property
+    def _api_key(self):
+        return self.__api_key
+    
+    @_api_key.getter
+    def _api_key(self):
+        return self.__api_key
+    
+    @_api_key.setter
+    def _api_key(self, value):
+        if not isinstance(value, str):
+            raise TypeError("API key must be a string!")
+        self.__api_key = value
+
     def __new__(cls) -> 'basicClient':
         return super().__new__(cls)
-
-    def __init__(self, config = None) -> None:
-        pass
     
     def apiError(self):
         print(f"One of api keys for class {self.__class__.__name__} is missing!")
@@ -37,6 +51,7 @@ class basicClient:
 class yahooClient(basicClient):
 
     def getTicket(self, ticket: str = None, start=dt.datetime.today(), end=dt.datetime.today()):
+        result = None
         if super().getTicket(ticket) != None:
             ticker = yf.Ticker(ticket)
             data = ticker.get_info()
@@ -53,24 +68,21 @@ class yahooClient(basicClient):
 class kucoinClient(basicClient):
     
     def __init__(self, config = None) -> None:
-        result = False
         if config != None:
-            StockApi = namedtuple('StockApi', ['key', 'secret', 'passphrase', 'sandbox'])
+            StockApi = namedtuple('StockApi', [ 'secret', 'passphrase', 'sandbox'])
             self.__api = StockApi()
             try:
-                self.__api.key = config["api_key"]
+                self._api_key = config["api_key"]
                 self.__api.secret = config["api_secret"]
                 self.__api.passphrase = config["api_passphrase"]
                 self.__api.sandbox = config["sandbox"]
             except:
                 super().apiError(self)
                 
-            self.__client = kc(self.__api.key, self.__api.secret, 
+            self.__client = kc(self._api_key, self.__api.secret, 
                                 self.__api.passphrase, self.__api.sandbox)
-            if self.__client != None:
-                result = True
-
-        return result
+            assert(self.__client != None)
+            
     
     def getTicket(self, ticket: str = None):
         result = None
@@ -85,15 +97,22 @@ class kucoinClient(basicClient):
                     print("Kucoin client get_currency request failed cause of" 
                                                                     "API error!")
                 
-        return result
-    
     def getTickets(self, tickets=any, start=dt.datetime.now(), end=dt.datetime.now()):
         # using basicClass API
         return super().getTickets(tickets, start, end)
     
+class coinmarketcapClient(basicClient):
+    def __init__(self, config=None) -> None:
+        super().__init__(config)
     
+    def getTicket(self, ticket: str = None):
+        return super().getTicket(ticket)
+    
+    def getTickets(self, tickets=any, start=dt.datetime.now(), end=dt.datetime.now()):
+        return super().getTickets(tickets, start, end)
 
-stockClients = {"yahoo": yahooClient, "kucoin": kucoinClient}
+stockClients = {"yahoo": yahooClient, "kucoin": kucoinClient, 
+                "coinmarketcup": coinmarketcapClient}
 
 class stockExchange:
     def __init__(self, stock) -> None:
@@ -139,3 +158,4 @@ class stockClient:
         for stock in self.__stocks:
             if stock.client != None:
                 return stock.getStockPrices()
+
--- tests.py
+++ tests.py
@@ -1,48 +1,67 @@
 import unittest
-from unittest.mock import MagicMock
-from finance import *
+from unittest.mock import MagicMock, patch
+import datetime as dt
+from finance import basicClient, yahooClient, kucoinClient, coinmarketcapClient, stockClients, stockExchange, stockClient
 
-class TestBasicClient(unittest.TestCase):
-    def test_getTicket(self):
-        client = basicClient()
-        self.assertIsNone(client.getTicket())
-        self.assertEqual(client.getTicket("AAPL"), "AAPL")
+class TestClients(unittest.TestCase):
+
+    def setUp(self):
+        self.basic_client = basicClient()
+        self.yahoo_client = yahooClient()
+        self.kucoin_client = kucoinClient()
+        self.coinmarketcap_client = coinmarketcapClient()
+
+    def test_basic_client_api_key(self):
+        with self.assertRaises(TypeError):
+            self.basic_client._api_key = 123
     
-    def test_getTickets(self):
-        client = basicClient()
-        tickets = [{"name": "Apple", "ticket": "AAPL"}, {"name": "Microsoft", "ticket": "MSFT"}]
-        expected = [namedtuple('Ticket', ['name', 'ticketData'])("Apple", "AAPL"), 
-                    namedtuple('Ticket', ['name', 'ticketData'])("Microsoft", "MSFT")]
-        self.assertEqual(client.getTickets(tickets), expected)
-        
-class TestYahooClient(unittest.TestCase):
-    def test_getTicket(self):
-        client = yahooClient()
-        client.getTicket = MagicMock(return_value=(100, {"regularMarketOpen": 100}))
-        self.assertEqual(client.getTicket("AAPL"), (100, {"regularMarketOpen": 100}))
-        client.getTicket.assert_called_once_with("AAPL")
+    def test_getTicket_with_valid_ticket(self):
+        self.yahoo_client.getTicket = MagicMock(return_value=(10, {'data': 'test_data'}))
+        self.assertEqual(self.yahoo_client.getTicket("AAPL"), (10, {'data': 'test_data'}))
+
+    def test_getTicket_with_none_ticket(self):
+        self.assertIsNone(self.yahoo_client.getTicket())
+
+    def test_kucoin_client_init_success(self):
+        kucoin_client = kucoinClient(config={'api_key': 'test_api_key', 'api_secret': 'test_secret', 'api_passphrase': 'test_pass', 'sandbox': True})
+        self.assertEqual(kucoin_client._api_key, 'test_api_key')
+
+    def test_kucoin_client_init_failure(self):
+        kucoin_client = kucoinClient(config={'api_key': 'test_api_key'})
+        self.assertIsNone(kucoin_client._kucoinClient__client)
     
-    def test_getTicket_no_price(self):
-        client = yahooClient()
-        client.getTicket = MagicMock(return_value=(None, {"regularMarketOpen": None}))
-        self.assertEqual(client.getTicket("AAPL"), (None, {"regularMarketOpen": None}))
-        client.getTicket.assert_called_once_with("AAPL")
-        
+    def test_getTickets_with_any_tickets(self):
+        self.assertEqual(self.coinmarketcap_client.getTickets(), [])
+
+    def test_getTickets_with_valid_tickets(self):
+        tickets = [{'name': 'AAPL', 'ticket': 'AAPL'}, {'name': 'GOOG', 'ticket': 'GOOG'}, {'name': 'TSLA', 'ticket': 'TSLA'}]
+        self.assertEqual(self.yahoo_client.getTickets(tickets), [('AAPL', None), ('GOOG', None), ('TSLA', None)])
+
 class TestStockExchange(unittest.TestCase):
+
+    def setUp(self):
+        self.yahoo_exchange = stockExchange({'name': 'yahoo', 'tickets': [{'name': 'AAPL', 'ticket': 'AAPL'}]})
+        self.invalid_exchange = stockExchange({'name': 'test_exchange', 'tickets': [{'name': 'test_ticket', 'ticket': 'TEST_TICKET'}]})
+
+    def test_yahoo_exchange_init(self):
+        self.assertIsNotNone(self.yahoo_exchange.client)
+
+    def test_invalid_exchange_init(self):
+        self.assertIsNone(self.invalid_exchange.client)
+
     def test_getStockPrices(self):
-        client = stockExchange("yahoo", ["AAPL", "MSFT"])
-        client.client.getTickets = MagicMock(return_value=[namedtuple('Ticket', ['name', 'ticketData'])("Apple", (100, {"regularMarketOpen": 100})),
-        namedtuple('Ticket', ['name', 'ticketData'])("Microsoft", (200, {"regularMarketOpen": 200}))])
-        self.assertEqual(client.getStockPrices(), [100, 200])
-        client.client.getTickets.assert_called_once_with(tickets=["AAPL", "MSFT"])
+        self.yahoo_exchange.client.getTickets = MagicMock(return_value=[('AAPL', (10, {'data': 'test_data'}))])
+        self.assertEqual(self.yahoo_exchange.getStockPrices(), [10])
 
 class TestStockClient(unittest.TestCase):
-    def test_getCurrentPrices(self):
-        client = stockClient()
-        client.stocks = [stockExchange("yahoo", ["AAPL", "MSFT"])]
-        client.stocks[0].getStockPrices = MagicMock(return_value=[100, 200])
-        self.assertEqual(client.getCurrentPrices(), [100, 200])
-        client.stocks[0].getStockPrices.assert_called_once()
+
+    @patch('my_module.os.path.dirname')
+    @patch('my_module.js.load')
+    def test_init_stockClient(self, mock_js_load, mock_dirname):
+        mock_dirname.return_value = '/test'
+        mock_js_load.return_value = {'stock_exchange': [{'name': 'yahoo','tickets': [{'name': 'AAPL','ticket': 'AAPL'}]}]}
+        stock_client = stockClient()
+        self.assertEqual(stock_client.getCurrentPrices(), [10])
 
 if __name__ == '__main__':
     unittest.main()
