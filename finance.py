68189d4f2d3c5dd8a6b8c93d6d738b4953a32a09
kom46 <kommunizm46@gmail.com>
2023-05-07 19:22:30 +0300

    added poetry project mangement; added some refactoring;

--- finance.py
+++ finance.py
@@ -4,29 +4,28 @@ import yfinance as yf
 import json as js
 import datetime as dt
 from kucoin.client import Client as kc
-from kucoin.exceptions import *
-from array import *
-
+from kucoin.exceptions import KucoinAPIException, KucoinRequestException
+from typing import List, Any, Dict
 
 
 class basicClient:
-    __tickers = []
-    
+    __tickers: List[Any] = []
+
     @property
     def tickers(self):
         return self.__tickers
-    
+
     @tickers.setter
-    def tickers(self, value = None):
+    def tickers(self, value=None):
         if value == None:
             return
-        
+
         if isinstance(value, list):
-        # if value is array
+            # if value is array
             for t in value:
                 self.__update_ticker(t)
         else:
-        # if it is one ticket
+            # if it is one ticket
             self.__update_ticker(value)
         # sort tickers by name
         print(self.__tickers)
@@ -42,18 +41,18 @@ class basicClient:
                     found = True
         if not found:
             self.__tickers.append(value)
-    
-    def __init__(self, config = None, api_key = "") -> None:
-        self.__api_key  = api_key
-    
+
+    def __init__(self, config=None, api_key="") -> None:
+        self.__api_key = api_key
+
     @property
     def _api_key(self):
         return self.__api_key
-    
+
     @_api_key.getter
     def _api_key(self):
         return self.__api_key
-    
+
     @_api_key.setter
     def _api_key(self, value):
         if not isinstance(value, str):
@@ -62,11 +61,12 @@ class basicClient:
 
     def __new__(cls) -> 'basicClient':
         return super().__new__(cls)
-    
+
     def apiError(self):
-        print(f"One of api keys for class {self.__class__.__name__} is missing!")
-    
-    def getTicket(self, ticket: str  = ""):
+        print(
+            f"One of api keys for class {self.__class__.__name__} is missing!")
+
+    def getTicket(self, ticket: str = ""):
         data = None
         if ticket != "":
             data = ticket
@@ -83,81 +83,89 @@ class basicClient:
 
 class yahooClient(basicClient):
 
-    def getTicket(self, ticket: str = None, start=dt.datetime.today(), end=dt.datetime.today()):
+    def getTicket(self, ticket: str = "", start=dt.datetime.today(), end=dt.datetime.today()):
         result = None
-        if super().getTicket(ticket) != None:
+        if super().getTicket(ticket) != "":
             ticker = yf.Ticker(ticket)
             data = ticker.get_info()
             # data = ticker.history(start=start, end=end)
-            price = None
-            try:
-                price = data["regularMarketOpen"]
-            except:
-                pass
+            # price = None
+            # try:
+            #     price = data["regularMarketOpen"]
+            # except:
+            #     pass
             result = data
         return result
 
 # TODO: implement kucoin crypto exchange
+
+
 class kucoinClient(basicClient):
-    
-    def __init__(self, config = None) -> None:
-        if config != None:
-            StockApi = namedtuple('StockApi', [ 'secret', 'passphrase', 'sandbox'])
-            self.__api = StockApi()
+
+    def __init__(self, config: Dict[Any, Any] = {}) -> None:
+        if len(config) > 0:
+            StockApi = namedtuple(
+                'StockApi', ['secret', 'passphrase', 'sandbox'])
             try:
                 self._api_key = config["api_key"]
-                self.__api.secret = config["api_secret"]
-                self.__api.passphrase = config["api_passphrase"]
-                self.__api.sandbox = config["sandbox"]
-            except:
-                super().apiError(self)
-                
-            self.__client = kc(self._api_key, self.__api.secret, 
-                                self.__api.passphrase, self.__api.sandbox)
-            
-            assert(self.__client != None)
-            
-    
-    def getTicket(self, ticket: str = None):
+                self.__api = StockApi(
+                    secret=config["api_secret"],
+                    passphrase=config["api_passphrase"],
+                    sandbox=config["sandbox"])
+            except Exception as e:
+                if isinstance(e, KucoinAPIException) or isinstance(e, KucoinRequestException):
+                    self.apiError()
+
+            self.__client = kc(self._api_key, self.__api.secret,
+                               self.__api.passphrase, self.__api.sandbox)
+
+            assert (self.__client is not None)
+
+    def getTicket(self, ticket: str = ""):
         result = None
-        if super().getTicket(ticket) != None:
+        if super().getTicket(ticket) is not None:
             try:
                 result = self.__client.get_ticker(ticket)
             except Exception as e:
                 if isinstance(e, KucoinRequestException):
-                    print("Kucoin client get_currency request failed cause of" 
-                                                                "network error!")
+                    print("Kucoin client get_currency request failed cause of"
+                          "network error!")
                 if isinstance(e, KucoinAPIException):
-                    print("Kucoin client get_currency request failed cause of" 
-                                                                    "API error!")
+                    print("Kucoin client get_currency request failed cause of"
+                          "API error!")
         return result
-                
+
     def getTickets(self, tickets=any):
         # using basicClass API
         return super().getTickets(tickets)
-    
+
+
 class coinmarketcapClient(basicClient):
     def __init__(self, config=None) -> None:
         super().__init__(config)
-    
-    def getTicket(self, ticket: str = None):
+
+    def getTicket(self, ticket: str = ""):
         return super().getTicket(ticket)
-    
+
     def getTickets(self, tickets=any):
         return super().getTickets(tickets)
 
-stockClients = {"yahoo": yahooClient, "kucoin": kucoinClient, 
+
+stockClients = {"yahoo": yahooClient, "kucoin": kucoinClient,
                 "coinmarketcap": coinmarketcapClient}
+
 
 class stockExchange:
     def __init__(self, stock) -> None:
+        # TODO: add try except block
         self.__name = stock["name"]
         self.__tickets = stock["tickets"]
-        if self.__name != None:
+        if self.__name is not None:
             try:
                 self.__client = stockClients[self.__name]()
             except KeyError:
                 self.__client = None
+
     @property
     def client(self):
         return self.__client
@@ -191,5 +199,3 @@ class stockClient:
             if stock.client != None:
                 result.append(stock.getStockPrices())
         return result
-
-
--- tests.py
+++ tests.py
@@ -2,41 +2,45 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 from finance import (
-    basicClient, 
-    yahooClient, 
-    kucoinClient, 
+    basicClient,
+    yahooClient,
+    kucoinClient,
     coinmarketcapClient,
-    stockExchange, 
+    stockExchange,
     stockClient,
     stockClients
 )
 
+
 class TestBasicClient(unittest.TestCase):
     def test_tickers_property(self):
         client = basicClient()
-        client.tickers = [{"name": "Bitcoin", "symbol": "BTC"}, {"name": "Apple", "symbol": "AAPL"}]
-        self.assertEqual(client.tickers, [{"name": "Apple", "symbol": "AAPL"}, {"name": "Bitcoin", "symbol": "BTC"}])
-    
+        client.tickers = [{"name": "Bitcoin", "symbol": "BTC"}, {
+            "name": "Apple", "symbol": "AAPL"}]
+        self.assertEqual(client.tickers, [{"name": "Apple", "symbol": "AAPL"}, {
+                         "name": "Bitcoin", "symbol": "BTC"}])
+
     def test_tickers_property_with_array(self):
         client = basicClient()
         client.tickers = [{"name": "AAPL"}, {"name": "GOOG"}]
         self.assertEqual(client.tickers, [{"name": "AAPL"}, {"name": "GOOG"}])
-    
+
     def test_tickers_property_with_array_should_sort_by_name(self):
         client = basicClient()
         client.tickers = [{"name": "GOOG"}, {"name": "AAPL"}]
         self.assertEqual(client.tickers, [{"name": "AAPL"}, {"name": "GOOG"}])
-    
+
     def test_get_ticket(self):
         client = basicClient()
         self.assertEqual(client.getTicket("AAPL"), None)
-        
+
     def test_get_tickets(self):
         client = basicClient()
         tickets = [{"name": "AAPL", "symbol": "AAPL"}]
         client.tickers = tickets
-        self.assertEqual(client.getTickets(tickets), [client.getTicket("AAPL")])
-    
+        self.assertEqual(client.getTickets(tickets),
+                         [client.getTicket("AAPL")])
+
 
 class TestYahooClient(unittest.TestCase):
     def test_get_ticket(self):
@@ -44,15 +48,18 @@ class TestYahooClient(unittest.TestCase):
         client.getTicket = MagicMock(return_value={"test": "test"})
         self.assertEqual(client.getTicket("AAPL"), {"test": "test"})
 
+
 class TestKucoinClient(unittest.TestCase):
     def test_get_ticket_should_return_none(self):
         client = kucoinClient()
         self.assertEqual(client.getTicket("AAPL"), None)
 
+
 class TestCoinmarketcapClient(unittest.TestCase):
     def test_get_ticket(self):
         client = coinmarketcapClient()
         self.assertEqual(client.getTicket("AAPL"), None)
+
 
 class TestStockExchange(unittest.TestCase):
     @patch.object(stockClients, 'yahooClient', spec=yahooClient)
@@ -65,6 +72,7 @@ class TestStockExchange(unittest.TestCase):
     def test_get_stock_prices_with_none_client(self):
         client = stockExchange({"name": "yahoo", "tickets": []})
         self.assertEqual(client.getStockPrices(), [])
+
 
 class TestStockClient(unittest.TestCase):
     @patch.object(stockExchange, 'getStockPrices')
